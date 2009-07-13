a8b46750c2668b90ce646129af4d36f086545d08
Bodaniel Jeanes <me@bjeanes.com>
2009-07-13 13:38:27 +1000

    If code language isn't recognized, default to unstyled, instead of raising error

--- lib/enki_formatter.rb
+++ lib/enki_formatter.rb
@@ -4,16 +4,20 @@ class EnkiFormatter
       Lesstile.format_as_xhtml(
         text,
         :text_formatter => lambda { |text| RedCloth.new(CGI::unescapeHTML(text)).to_html },
-        :code_formatter => lambda { |code, lang| 
-          lang = lang.to_s.downcase
+        :code_formatter => lambda { |code, lang|
+            lang = lang.to_s.downcase
           
-          code = Uv.parse(CGI::unescapeHTML(code), "xhtml", lang.to_s.downcase, false, "railscasts")
-          code = code.gsub(/^<pre class="[a-zA-Z_-]+">/,%Q[<code lang="#{lang}"><pre>])
-          code = code.gsub(/<\/pre>$/,'</pre></code>')
+            begin
+              code = Uv.parse(CGI::unescapeHTML(code), "xhtml", lang.to_s.downcase, false, "railscasts")
+              code = code.gsub(/^<pre class="[a-zA-Z_-]+">/,%Q[<code lang="#{lang}"><pre>])
+              code = code.gsub(/<\/pre>$/,'</pre></code>')
+            rescue
+              code = %Q{<code lang="#{lang}"><pre>#{code}</pre></code>}
+            end
 
-          lines = (1...(code.split(/\n/).size)).to_a.join("\n")
+            lines = (1...(code.split(/\n/).size)).to_a.join("\n")
           
-          "<table><caption>#{lang.titleize}</caption><tr><td><pre>#{lines}</pre></td><td>#{code}</td></tr></table>"
+            "<table><caption>#{lang.titleize}</caption><tr><th><pre>#{lines}</pre></th><td>#{code}</td></tr></table>"
         }
       )
     end
