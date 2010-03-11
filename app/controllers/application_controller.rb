ccf8c9d1954326368d0990d8f4cbbc8be30ac300
Bodaniel Jeanes <me@bjeanes.com>
2010-03-11 23:04:34 +1000

    redirect to main domain if on www.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,15 @@
 class ApplicationController < ActionController::Base
   include ExceptionNotifiable
 
-  helper :all # include all helpers, all the time
+  before_filter :ensure_domain
   after_filter :set_content_type
+
+  helper :all
   tab :home
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-#  protect_from_forgery :secret => 'a6a9e417376364b61645d469f04ac8cf'
+  #  protect_from_forgery :secret => 'a6a9e417376364b61645d469f04ac8cf'
 
   protected
 
@@ -21,5 +23,14 @@ class ApplicationController < ActionController::Base
 
   def set_content_type
     headers['Content-Type'] ||= 'application/xhtml+xml; charset=utf-8'
+  end
+
+  def ensure_domain
+    return unless Rails.env.production?
+    
+    domain = "bjeanes.com"
+    if request.env['HTTP_HOST'] != domain
+      redirect_to "http://#{domain}"
+    end
   end
 end
