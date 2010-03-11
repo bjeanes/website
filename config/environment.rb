c144d92aebbacfc71ba78dbd4822372d7e29305f
Bodaniel Jeanes <me@bjeanes.com>
2010-03-11 22:36:46 +1000

    Fix gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   config.gem "chronic", :version => "~> 0.2.0"
   config.gem "bjeanes-ultraviolet", :version => "0.10.3", :lib => "uv"
   config.gem "lesstile", :version => "~> 0.3"
-  config.gem "mislav-will_paginate", :lib => "will_paginate", :version => "~> 2.3", :github => 'http://gems.github.com'
+  config.gem "will_paginate", :version => "~> 2.3", :github => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
